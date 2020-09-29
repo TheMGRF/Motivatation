9d58a9f45a1434b9d627d3b0883f5d66a27a4604
TheMGRF <thomas.grf@googlemail.com>
2020-09-29 22:28:09 +0100

    Page variables

--- src/main/java/me/themgrf/motivatation/controllers/MainController.java
+++ src/main/java/me/themgrf/motivatation/controllers/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-    private static final String PAGE_NAME = "Home";
+    private static final String PAGE_NAME = "Motivatation | Home";
 
     private boolean loggedIn = false;
 
