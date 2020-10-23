b6fa39878f40dc7cf727c529566654757a0bb02f
TheMGRF <thomas.grf@googlemail.com>
2020-10-23 14:19:09 +0100

    Improve security and tidy upÂ

--- src/main/java/me/themgrf/motivatation/util/config/WebSecurityConfig.java
+++ src/main/java/me/themgrf/motivatation/util/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package me.themgrf.motivatation.util.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,20 +10,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] ALLOWED_PAGES = {"/", "/news", "/contact", "/signup"};
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/signup").permitAll()
+                .antMatchers(ALLOWED_PAGES).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/home", true)
+                .failureUrl("/login?error=true")
                 .permitAll()
                 .and()
                 .logout()
@@ -30,8 +37,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+    }
+
+    @Override
     public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/css/**", "/img/**", "/js/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
---
+++ src/main/resources/static/js/loadParticles.js
@@ -0,0 +1,3 @@
+particlesJS.load('particles-js', '/js/particles.json', function() {
+    console.log('[INFO] Particles loaded successfully!');
+});
